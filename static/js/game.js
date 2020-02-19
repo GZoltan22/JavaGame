ec9abc92dd4828b5af513a24867a0db40c3e9438
werneerm <wmarcos25@gmail.com>
2020-02-19 13:50:09 +0100

    movement

--- static/js/game.js
+++ static/js/game.js
@@ -13,11 +13,12 @@
   'AWAY','BABY','BACK','BAD','BADLY','BAG','BALANCE','BALL',
   'BALLOON','BAND','BANK','BAR','BARE','BARK','BARN','BASE'];
 
+
+
 let actual_word1 = document.querySelector('.words1');
 let actual_word2 = document.querySelector('.words2');
 let actual_word3 = document.querySelector('.words3');
 let wordInput = document.querySelector('.input');
-
 actual_word1.innerHTML = random_word();
 actual_word2.innerHTML = random_word();
 actual_word3.innerHTML = random_word();
@@ -31,7 +32,7 @@ wordInput.addEventListener('input', (event)=> {
     let enemy_div = document.getElementById('enemy1');
     enemy_div.id = ('dead');
     enemy.id = ('dead');
-    enemy.src = "static/css/rip.png";
+    enemy.src = "/static/css/rip.png";
     win_check()
   }
 });
@@ -44,7 +45,7 @@ wordInput.addEventListener('input', (event)=> {
     let enemy_div = document.getElementById('enemy2');
     enemy_div.id = ('dead');
     enemy.id = ('dead');
-    enemy.src = "static/css/rip.png";
+    enemy.src = "/static/css/rip.png";
     win_check()
     }
   });
@@ -57,7 +58,7 @@ wordInput.addEventListener('input', (event)=> {
     let enemy_div = document.getElementById('enemy3');
     enemy_div.id = ('dead');
     enemy.id = ('dead');
-    enemy.src = "static/css/rip.png";
+    enemy.src = "/static/css/rip.png";
     win_check()
     }
   });
@@ -75,3 +76,76 @@ wordInput.addEventListener('input', (event)=> {
     return list[random_index]
   }
 
+
+document.getElementById("player").focus();
+document.getElementById("player").select();
+
+ function moveEnemy() {
+  moveZombie1();
+  setTimeout(function () {
+    moveZombie2();
+  },2000)
+  setTimeout(function () {
+    moveZombie3();
+  },4000)
+ }
+
+
+ function moveZombie1() {
+  let zombie1 = document.getElementById("enemy1");
+  let pos = 0;
+  let zombie1IsDead = document.getElementById("first_enemy");
+  let id = setInterval(frame, 5);
+  function frame() {
+      if (pos === 1600) {
+        gameOver();
+      }
+      else if (zombie1IsDead.id === ('dead')) {
+        clearInterval(frame);
+      }
+      else {
+        pos = pos + 2;
+        zombie1.style.right = pos + "px";
+      }
+    }
+ }
+
+ function moveZombie2() {
+    let zombie2 = document.getElementById("enemy2");
+    let zombie2IsDead = document.getElementById("second_enemy");
+    let pos = 0;
+    let id = setInterval(frame,5);
+    function frame() {
+      if (pos === 1600) {
+        gameOver();
+      }
+      else if (zombie2IsDead.id === ('dead')) {
+        clearInterval(frame);
+      }
+      else {
+        pos = pos + 1;
+        zombie2.style.right = pos + "px";
+      }
+    }
+ }
+ function moveZombie3() {
+    let zombie3 = document.getElementById("enemy3");
+    let zombie3IsDead = document.getElementById("third_enemy");
+    let pos = 0;
+    let id = setInterval(frame,5);
+    function frame() {
+      if (pos === 1600) {
+        gameOver();
+      }
+      else if (zombie3IsDead.id === ('dead')) {
+        clearInterval(frame);
+      }
+      else {
+        pos = pos + 1;
+        zombie3.style.right = pos + "px";
+      }
+    }
+ }
+ function gameOver(){
+   alert('GAME OVER')
+ }
